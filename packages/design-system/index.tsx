e03cce0a835deff06c105ebdf2f4ec2a261a77ea
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-10-19 20:22:41 -0300

    chore(design-system): creating index file to export all the components

---
+++ packages/design-system/index.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+export * from './components/PokemonCard';
+export * from './components/PokemonTypeBadge';
+export * from './components/Search'
+export * from './components/primitives/Box'
+export * from './components/primitives/Text'
+export * from './components/primitives/Heading'
+export * from './components/primitives/Image'
+export * from './components/primitives/Input'
+export * from './styles/globalStyles'
