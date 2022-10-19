1a0c7d2612d0cacaffed53e98c4b24f129c05029
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-10-19 20:22:22 -0300

    feat(design-system): pokemon card component

---
+++ packages/design-system/components/PokemonCard.tsx
@@ -0,0 +1,40 @@
+import { Pokemon } from 'ui-rest-types';
+import { PokemonTypeBadge } from './PokemonTypeBadge';
+import { Box } from './primitives/Box';
+import { Heading } from './primitives/Heading';
+import { Image } from './primitives/Image';
+import { slateDark } from '@radix-ui/colors';
+import { Text } from './primitives/Text';
+
+type PokemonProps = { pokemon: Pokemon };
+export const PokemonCard = ({ pokemon }: PokemonProps) => {
+  return (
+    <Box
+      css={{
+        gap: '$2',
+        maxHeight: 'fit-content',
+        backgroundColor: slateDark.slate4,
+        padding: '15px 25px 15px 25px',
+      }}
+    >
+      <Heading size="small">{pokemon.name}</Heading>
+      <Image
+        src={pokemon.image_url}
+        alt={pokemon.name}
+        css={{
+          maxHeight: '10rem',
+          maxWidth: '15rem',
+        }}
+      />
+      <Box css={{ flexDirection: 'row', gap: '$4' }}>
+        <Box css={{ flexDirection: 'row', gap: '$1' }}>
+          <Text color="neutral-light">tipo:</Text>
+          <PokemonTypeBadge type={pokemon.info.type} />
+        </Box>
+        <Text color="neutral-light">
+          qtd. evoluções: {pokemon.evolutions?.length}
+        </Text>
+      </Box>
+    </Box>
+  );
+};
