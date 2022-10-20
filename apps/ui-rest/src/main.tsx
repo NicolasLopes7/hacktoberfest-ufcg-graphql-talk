32a80e4a96a71c38d653fb937ee2c4c0dcf4747d
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-10-19 21:41:09 -0300

    feat(prisma): creating new package for prisma

--- apps/ui-rest/src/main.tsx
+++ apps/ui-rest/src/main.tsx
@@ -7,6 +7,6 @@ globalStyles();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-      <App />
+    <App />
   </React.StrictMode>
 );
---
+++ packages/prisma/index.ts
@@ -0,0 +1,8 @@
+export * from '@prisma/client/index';
+
+import { PrismaClient } from '@prisma/client';
+
+//@ts-ignore
+export const prisma = global.prisma || new PrismaClient();
+//@ts-ignore
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
