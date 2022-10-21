d1898d1a76384c5b097ef51d058fa2dac8adaaff
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-10-21 20:57:49 -0300

    feat(api-rest): add search

--- apps/api-rest/src/modules/pokemon/controllers/getAll.ts
+++ apps/api-rest/src/modules/pokemon/controllers/getAll.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from 'express';
 import { prisma } from 'prisma';
 export const getAll = async (req: Request, res: Response) => {
-  const { page, limit } = req.query;
+  const { page, limit, search } = req.query;
+
+  const where: any = {};
+
+  if (search) {
+    where.name = { contains: search };
+  }
 
   const pokemons = await prisma.pokemon.findMany({
+    where,
     skip: page ? (Number(page) - 1) * Number(limit) : undefined,
     take: limit ? Number(limit) : undefined,
   });
